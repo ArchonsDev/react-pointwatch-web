165dbe057c312848371221509e7aa66d8e237ac6
Dawn <dydawnraine@gmail.com>
2024-04-09 16:57:01 +0800

    added icons library, modified app.js

---
+++ src/pages/login/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./style.module.css";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>login page</h1>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/registration/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./style.module.css";
+
+const Registration = () => {
+  return (
+    <div>
+      <h1>register page</h1>
+    </div>
+  );
+};
+
+export default Registration;
