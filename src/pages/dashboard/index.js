9d56b1c3602416f9a3dddd8426df6cc656bf95ec
Dawn <dydawnraine@gmail.com>
2024-10-02 16:52:53 +0800

    fix: head dashboard
    modified loading logic when there are no employees for the head's department

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -17,6 +17,7 @@ const Dashboard = () => {
   const navigate = useNavigate();
 
   const [departmentUsers, setDepartmentUsers] = useState([]);
+  const [noUsers, setNoUsers] = useState(true);
   const [terms, setTerms] = useState([]);
   const [selectedTerm, setSelectedTerm] = useState(null);
   const [userClearanceStatus, setUserClearanceStatus] = useState([]);
@@ -42,11 +43,17 @@ const Dashboard = () => {
         token: token,
       },
       (response) => {
-        setDepartmentUsers(
-          response.users.filter(
-            (us) => us.department === user.department && us.id !== user.id
-          )
+        const filteredUsers = response.users.filter(
+          (us) => us.department === user?.department && us.id !== user.id
         );
+
+        if (filteredUsers.length !== 0) {
+          setNoUsers(false);
+          setDepartmentUsers(filteredUsers);
+          setLoading(true);
+        } else {
+          setLoading(false);
+        }
       },
       (error) => {
         console.log(error);
@@ -106,9 +113,7 @@ const Dashboard = () => {
                 swtds: filteredSWTDs,
               },
             }));
-
             setRequiredPoints(clearanceResponse.points.required_points);
-            setLoading(false);
           },
           (error) => {
             console.log(
@@ -187,9 +192,10 @@ const Dashboard = () => {
       if (!user?.is_admin && !user?.is_staff && !user?.is_superuser)
         navigate("/swtd");
       else {
+        setLoading(true);
         const fetchData = async () => {
-          await fetchAllUsers();
           fetchTerms();
+          await fetchAllUsers();
         };
         fetchData();
       }
@@ -201,6 +207,14 @@ const Dashboard = () => {
       fetchClearanceStatusForAllUsers(selectedTerm);
     }
   }, [selectedTerm]);
+
+  useEffect(() => {
+    if (
+      !noUsers &&
+      Object.values(userClearanceStatus).length === departmentUsers.length
+    )
+      setLoading(false);
+  }, [userClearanceStatus, departmentUsers]);
 
   if (loading)
     return (
@@ -216,7 +230,7 @@ const Dashboard = () => {
       <Row className="mb-2 w-100">
         <Col>
           <h3 className={styles.label}>
-            {user.department} Department Dashboard
+            {user?.department} Department Dashboard
           </h3>
         </Col>
         <Col
@@ -248,187 +262,208 @@ const Dashboard = () => {
         </Col>
       </Row>
 
-      <Row className="w-100 mb-3">
-        <Col md="7">
-          <Card className={styles.blackCard}>
-            <Card.Body>
-              <Row>
-                <Col
-                  className={`${styles.cardCol1} d-flex justify-content-center align-items-center flex-column p-2`}
-                  md="5">
-                  <Row className="text-center">
-                    % of employees lacking points
-                  </Row>
-                  <Row className={styles.lackingPercent}>
-                    {lackingUsersPercentage}%
-                  </Row>
-                  <Row className="w-100">
-                    <Col>
-                      <ProgressBar
-                        className={styles.bar}
-                        now={lackingUsersPercentage}
-                      />
-                    </Col>
-                  </Row>
-                </Col>
-                <Col className={`${styles.depCol}`}>
-                  <span className={`${styles.depTitle} mb-3`}>
-                    Department Statistics
-                  </span>
-                  <hr className="m-0 mb-2" style={{ opacity: "1" }} />
-                  <Row className={`${styles.depStat} w-100 mb-2`}>
-                    <Col md="auto">Total Employees</Col>
-                    <Col className="text-end">
-                      {Object.values(userClearanceStatus).length}
-                    </Col>
-                  </Row>
-                  <Row className={`${styles.depStat} w-100 mb-2`}>
-                    <Col md="auto">Employees Lacking Points</Col>
-                    <Col className="text-end">{lackingUsers}</Col>
-                  </Row>
-                  <Row className={`${styles.depStat} w-100`}>
-                    <Col md="auto">Employees with Required Points</Col>
-                    <Col className="text-end">{validUsers}</Col>
-                  </Row>
-                </Col>
-              </Row>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <span className={styles.formLabel}>
-            Point Standings for {selectedTerm?.name}
-          </span>
-          <hr className="m-0 mb-1" style={{ opacity: "1" }} />
-          {topUsers.map((user) => (
-            <Row className={styles.cardBody} key={user.userId}>
-              <Col md="1">{user.employee_id}</Col>
-              <Col>
-                {user.firstname} {user.lastname}
-              </Col>
-              <Col className="text-end">
-                {user.points?.valid_points || 0} {/* Display valid points */}
-              </Col>
-            </Row>
-          ))}
-        </Col>
-      </Row>
-
-      <Row className="w-100">
-        <Col className="text-start" md={7}>
-          <InputGroup className={`${styles.searchBar} mb-3`}>
-            <InputGroup.Text>
-              <i className="fa-solid fa-magnifying-glass"></i>
-            </InputGroup.Text>
-            <Form.Control
-              type="search"
-              placeholder="Search an employee by ID number, firstname, or lastname."
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-          </InputGroup>
-        </Col>
-      </Row>
-
-      <Row className="w-100">
-        {!userClearanceStatus ? (
-          <span
-            className={`${styles.msg} d-flex justify-content-center align-items-center mt-5 w-100`}>
-            No employees found.
-          </span>
-        ) : (
-          <div className="mb-3">
-            <ListGroup className="w-100" variant="flush">
-              <ListGroup.Item className={styles.tableHeader}>
-                <Row>
-                  <Col md={2}>ID No.</Col>
-                  <Col>Name</Col>
-                  <Col className="text-center" md={2}>
-                    Pending SWTDs
-                  </Col>
-                  <Col className="text-center" md={2}>
-                    Approved SWTDs
-                  </Col>
-                  <Col className="text-center" md={2}>
-                    SWTDs For Revision
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-            </ListGroup>
-            <ListGroup>
-              {currentRecords.map((item) => (
-                <ListGroup.Item
-                  key={item.employee_id}
-                  className={styles.tableBody}
-                  onClick={() => handleEmployeeSWTDClick(item.id)}>
+      {/* APPEAR IF HAVE USERS */}
+      {!noUsers && (
+        <>
+          <Row className="w-100 mb-3">
+            <Col md="7">
+              <Card className={styles.blackCard}>
+                <Card.Body>
                   <Row>
-                    <Col md={2}>{item.employee_id}</Col>
-                    <Col>
-                      {item.firstname} {item.lastname}
+                    <Col
+                      className={`${styles.cardCol1} d-flex justify-content-center align-items-center flex-column p-2`}
+                      md="5">
+                      <Row className="text-center">
+                        % of employees lacking points
+                      </Row>
+                      <Row className={styles.lackingPercent}>
+                        {lackingUsersPercentage ? lackingUsersPercentage : "0"}%
+                      </Row>
+                      <Row className="w-100">
+                        <Col>
+                          <ProgressBar
+                            className={styles.bar}
+                            now={lackingUsersPercentage}
+                          />
+                        </Col>
+                      </Row>
                     </Col>
-                    <Col className="text-center" md={2}>
-                      {
-                        item.swtds.filter(
-                          (swtd) => swtd.validation.status === "PENDING"
-                        ).length
-                      }
-                    </Col>
-                    <Col className="text-center" md={2}>
-                      {
-                        item.swtds.filter(
-                          (swtd) => swtd.validation.status === "APPROVED"
-                        ).length
-                      }
-                    </Col>
-                    <Col className="text-center" md={2}>
-                      {
-                        item.swtds.filter(
-                          (swtd) => swtd.validation.status === "REJECTED"
-                        ).length
-                      }
+                    <Col className={`${styles.depCol}`}>
+                      <span className={`${styles.depTitle} mb-3`}>
+                        Department Statistics
+                      </span>
+                      <hr className="m-0 mb-2" style={{ opacity: "1" }} />
+                      <Row className={`${styles.depStat} w-100 mb-2`}>
+                        <Col md="auto">Total Employees</Col>
+                        <Col className="text-end">{departmentUsers.length}</Col>
+                      </Row>
+                      <Row className={`${styles.depStat} w-100 mb-2`}>
+                        <Col md="auto">Employees Lacking Points</Col>
+                        <Col className="text-end">{lackingUsers}</Col>
+                      </Row>
+                      <Row className={`${styles.depStat} w-100`}>
+                        <Col md="auto">Employees with Required Points</Col>
+                        <Col className="text-end">{validUsers}</Col>
+                      </Row>
                     </Col>
                   </Row>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          </div>
-        )}
-      </Row>
-      <Row className="w-100 mb-3">
-        <Col className="d-flex justify-content-center">
-          <Pagination>
-            <Pagination.First
-              className={styles.pageNum}
-              onClick={() => handlePageChange(1)}
-            />
-            <Pagination.Prev
-              className={styles.pageNum}
-              onClick={() => {
-                if (currentPage > 1) handlePageChange(currentPage - 1);
-              }}
-            />
-            {Array.from({ length: totalPages }, (_, index) => (
-              <Pagination.Item
-                key={index + 1}
-                active={index + 1 === currentPage}
-                className={styles.pageNum}
-                onClick={() => handlePageChange(index + 1)}>
-                {index + 1}
-              </Pagination.Item>
-            ))}
-            <Pagination.Next
-              className={styles.pageNum}
-              onClick={() => {
-                if (currentPage < totalPages) handlePageChange(currentPage + 1);
-              }}
-            />
-            <Pagination.Last
-              className={styles.pageNum}
-              onClick={() => handlePageChange(totalPages)}
-            />
-          </Pagination>
-        </Col>
-      </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            {noUsers === false && terms.length !== 0 && (
+              <Col>
+                <span className={styles.formLabel}>
+                  Point Standings for {selectedTerm?.name}
+                </span>
+                <hr className="m-0 mb-1" style={{ opacity: "1" }} />
+
+                {topUsers.map((user) => (
+                  <Row className={styles.cardBody} key={user.userId}>
+                    <Col md="1">{user.employee_id}</Col>
+                    <Col>
+                      {user.firstname} {user.lastname}
+                    </Col>
+                    <Col className="text-end">
+                      {user.points?.valid_points || 0}
+                    </Col>
+                  </Row>
+                ))}
+              </Col>
+            )}
+          </Row>
+
+          <Row className="w-100">
+            <Col className="text-start" md={7}>
+              <InputGroup className={`${styles.searchBar} mb-3`}>
+                <InputGroup.Text>
+                  <i className="fa-solid fa-magnifying-glass"></i>
+                </InputGroup.Text>
+                <Form.Control
+                  type="search"
+                  name="searchQuery"
+                  placeholder="Search an employee by ID number, firstname, or lastname."
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+              </InputGroup>
+            </Col>
+          </Row>
+
+          <Row className="w-100">
+            {currentRecords.length === 0 ? (
+              <span
+                className={`${styles.msg} d-flex justify-content-center align-items-center mt-3 mb-3 w-100`}>
+                No employees found.
+              </span>
+            ) : (
+              <div className="mb-3">
+                <ListGroup className="w-100" variant="flush">
+                  <ListGroup.Item className={styles.tableHeader}>
+                    <Row>
+                      <Col md={2}>ID No.</Col>
+                      <Col>Name</Col>
+                      <Col className="text-center" md={2}>
+                        Pending SWTDs
+                      </Col>
+                      <Col className="text-center" md={2}>
+                        Approved SWTDs
+                      </Col>
+                      <Col className="text-center" md={2}>
+                        SWTDs For Revision
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                </ListGroup>
+                <ListGroup>
+                  {currentRecords.map((item) => (
+                    <ListGroup.Item
+                      key={item.employee_id}
+                      className={styles.tableBody}
+                      onClick={() => handleEmployeeSWTDClick(item.id)}>
+                      <Row>
+                        <Col md={2}>{item.employee_id}</Col>
+                        <Col>
+                          {item.firstname} {item.lastname}
+                        </Col>
+                        <Col className="text-center" md={2}>
+                          {
+                            item.swtds.filter(
+                              (swtd) => swtd.validation.status === "PENDING"
+                            ).length
+                          }
+                        </Col>
+                        <Col className="text-center" md={2}>
+                          {
+                            item.swtds.filter(
+                              (swtd) => swtd.validation.status === "APPROVED"
+                            ).length
+                          }
+                        </Col>
+                        <Col className="text-center" md={2}>
+                          {
+                            item.swtds.filter(
+                              (swtd) => swtd.validation.status === "REJECTED"
+                            ).length
+                          }
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </div>
+            )}
+          </Row>
+
+          <Row className="w-100 mb-3">
+            <Col className="d-flex justify-content-center">
+              <Pagination>
+                <Pagination.First
+                  className={styles.pageNum}
+                  onClick={() => handlePageChange(1)}
+                />
+                <Pagination.Prev
+                  className={styles.pageNum}
+                  onClick={() => {
+                    if (currentPage > 1) handlePageChange(currentPage - 1);
+                  }}
+                />
+                {Array.from({ length: totalPages }, (_, index) => (
+                  <Pagination.Item
+                    key={index + 1}
+                    active={index + 1 === currentPage}
+                    className={styles.pageNum}
+                    onClick={() => handlePageChange(index + 1)}>
+                    {index + 1}
+                  </Pagination.Item>
+                ))}
+                <Pagination.Next
+                  className={styles.pageNum}
+                  onClick={() => {
+                    if (currentPage < totalPages)
+                      handlePageChange(currentPage + 1);
+                  }}
+                />
+                <Pagination.Last
+                  className={styles.pageNum}
+                  onClick={() => handlePageChange(totalPages)}
+                />
+              </Pagination>
+            </Col>
+          </Row>
+        </>
+      )}
+
+      {/* APPEAR IF NO USERS */}
+      {noUsers && (
+        <>
+          <hr className="w-100" style={{ opacity: "1" }} />
+          <span
+            className={`${styles.msg} d-flex justify-content-center align-items-center w-100`}>
+            No employees in this department yet.
+          </span>
+        </>
+      )}
     </Container>
   );
 };
